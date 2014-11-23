4afadc4b076665da96fbce0f2b7788a65874bf26
Scott Arciszewski <scott@arciszewski.me>
2014-11-23 17:05:47 -0500

    Use Twig for config template rendering.

--- src/public/website/index.php
+++ src/public/website/index.php
@@ -25,6 +25,21 @@ $router->inject(
 );
 
 
+// Let's initialize a loader
+$loader = new \Twig_Loader_Filesystem([ ONIMBUS_ROOT."/templates/" ]);
+
+// Now let's set up our Twig Environment
+if (!empty($config['templates']['cache'])) {
+    $twigEnv = new \Twig_Environment(
+        $loader,
+        [ 'cache' => $cache ]
+    );
+} else {
+    $twigEnv = new \Twig_Environment($loader);
+}
+
+// Inject our template engine
+$router->inject('template', $twigEnv);
 
 // Now, let's fire off the router
 
